85f1275768173a233e1e1c56941ff6b19a2436ee
Walter213 <46505629+Walter213@users.noreply.github.com>
2019-11-13 20:48:23 -0700

    Working With Security

---
+++ ChinookSolution/ChinookSystem/BLL/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Data.DTOs;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+using ChinookSystem.Data.Entites;
+using Chinook.Data.POCOs;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class EmployeeController
+    {
+        public List<string> Employees_GetTitles()
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = (from x in context.Employees
+                               select x.Title).Distinct();
+                return results.ToList();
+            }
+        }
+    }
+}
